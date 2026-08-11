51d24603cd4252409c6535d74f67dc08818556f0
RobertVinson <bbkkssv@gmail.com>
2026-08-11 10:35:57 -0500

    Initial Commit

---
+++ FlashCardCohort10/FlashCardCohort10App.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardCohort10App.swift
+//  FlashCardCohort10
+//
+//  Created by Robert Vinson on 8/11/26.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardCohort10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
